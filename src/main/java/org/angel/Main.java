ea40045e443d16c93b44df1e851fedac0e3b1311
Angel <angelaindahpuspita@gmail.com>
2025-02-17 13:08:07 +0800

    Initial commit

---
+++ src/main/java/org/angel/Main.java
@@ -0,0 +1,11 @@
+package org.angel;
+
+import com.google.common.collect.Lists;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Lists.newArrayList("Ayu", "Jery", "Charlie");
+        System.out.println(names);
+    }
+}
